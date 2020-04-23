2a75e8d4e429f75d2cd98f8ece30b541c4840859
annaweronica <anna.janiak@yahoo.pl>
2020-04-23 16:54:55 +0000

    display menu html and js done

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Each line should consist of:
 - index number starting form  1
 - name taken from file salads.json
 - price which has to be added accordingly to the pattern (if the dish fits to both then you take the first criterium):  
-    1. 15euro every third dish
-    2. 10euro dish beginns with letter "E", "R" lub "G"
-    3. 20euro dish which does not fit to any above
+    1. 15zl every third dish
+    2. 10zl dish beginns with letter "E", "R" lub "G"
+    3. 20zl dish which does not fit to any above
 - checkbox.
 
 The price sum must be displayed under the list. Suggested layout:
--- scripts.js
+++ scripts.js
@@ -1,7 +1,46 @@
+//global variable _name
+//var _SALADS;
+
 fetch("./salads.json")
     .then(function(resp) {
+        //return stop exectuitng the code and the vaule is returend and console log just print to console
         return resp.json();
     })
     .then(function(data) {
-        console.log(data);
+        //calling function populateMenu
+        populateMenu(data);
     }) 
+
+//promise - to check regarding waiting time to excatue the code
+//function body declaration
+function populateMenu(data){
+    console.log(data);  
+    for (i = 0; i < data.length; i++) {
+        let price = "20";
+        if ((i+1) % 3 == 0){
+        price = "15";
+        } else if (data[i].charAt(0) == "E" || data[i].charAt(0) == "R" || data[i].charAt(0) == "G"){
+        price = "10";
+        }     
+
+        let menuItem = "<tr data-price='"+price+"'>"; 
+        menuItem += "<td>" + (i+1).toString() + "</td>";
+        menuItem += "<td>" + data[i] + "</td>";
+        menuItem += "<td>" + price + "zl </td>";
+        menuItem += "<td><input type='checkbox'></td>";
+        menuItem += "</tr>";
+
+    document.getElementById("menu-table").innerHTML += menuItem;
+    
+    }
+}
+
+//onlick o checkbox
+//global var "total"
+//when thicked you add 
+//data attribute (parent attribute)
+
+
+
+
+
